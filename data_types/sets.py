1c627a6a8853b8405f92cbbda859667b4ab581b2
Nafio Nabi <nafionabi.work.acc@gmail.com>
2022-02-07 18:30:50 +0600

    Sets

---
+++ data_types/sets.py
@@ -0,0 +1,10 @@
+names = {"Erland Haaland", "Robert Lewandowski", "Erland Haaland", "Cristiano Ronaldo"}
+print(names)
+
+names.add("Harry Kane")
+print(names)
+
+last_name = names.pop()
+print(names)
+
+print(last_name)
