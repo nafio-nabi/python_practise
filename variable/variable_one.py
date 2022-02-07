4d6a38f23d917a5168b2664f22b79531a23a2c13
Nafio Nabi <nafionabi.work.acc@gmail.com>
2022-02-07 10:11:42 +0600

    Variable

---
+++ variable/variable_one.py
@@ -0,0 +1,8 @@
+first_name = "John"
+last_name = "Doe"
+age = 40
+weight = 59.99
+
+print(f"Name: {first_name} {last_name}")
+print(f"Age: {age}")
+print(f"Weight: {weight}")
---
+++ variable/variable_two.py
@@ -0,0 +1,9 @@
+num_one = 150
+num_two = 50
+sum = num_one + num_two
+print(sum)
+
+num_one = 150
+num_two = 150
+sum = num_one + num_two
+print(sum)
