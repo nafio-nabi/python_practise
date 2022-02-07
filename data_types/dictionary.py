1a9bb14c634b0a1673613532f577652336a2c1a5
Nafio Nabi <nafionabi.work.acc@gmail.com>
2022-02-07 19:03:06 +0600

    Dictionary

---
+++ data_types/dictionary.py
@@ -0,0 +1,13 @@
+company = {"Name": "Netflix", "Stock Name": "NFLX", "Stock Price": 410.17}
+print(company)
+
+print()
+
+for item, value in company.items():
+    print(f"{item}: {value}")
+
+print()
+
+print(company["Name"])
+print(company["Stock Name"])
+print(company["Stock Price"])
