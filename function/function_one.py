d0ae396ee6e481b85f7881735ae2f9ec929e323a
Nafio Nabi <nafionabi.work.acc@gmail.com>
2022-02-07 10:43:25 +0600

    Function

---
+++ function/function_one.py
@@ -0,0 +1,12 @@
+def addition(num_one, num_two):
+    sum = num_one + num_two
+    print(sum)
+
+print(addition(5, 5))
+
+def subtraction(num_one, num_two):
+    sub = num_one - num_two
+    print(sub)
+    return sub
+
+print(subtraction(10, 5))
---
+++ function/function_two.py
@@ -0,0 +1,5 @@
+def greet(first_message, last_message="Universe"):
+    return f"{first_message} {last_message}."
+
+print(greet("Hello", "World"))
+print(greet("Hello"))
