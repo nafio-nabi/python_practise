e1003b9996ed4845e20aeba3dff6918a0cc29833
Nafio Nabi <nafionabi.work.acc@gmail.com>
2022-02-07 09:38:52 +0600

    Arithmetic operations.

---
+++ data_types/arithmetic.py
@@ -0,0 +1,20 @@
+num_one = 10
+num_two = 5
+
+# Addition
+print(num_one + num_two)
+
+# Subtraction
+print(num_one - num_two)
+
+# Multiplication
+print(num_one * num_two)
+
+# Division
+print(num_one / num_two)
+
+# Floor division
+print(num_one // num_two)
+
+# Modulo division
+print(num_one % num_two)
