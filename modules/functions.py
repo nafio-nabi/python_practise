163d23dbff3287259765b88be95f405740ff3b15
Nafio Nabi <nafionabi.work.acc@gmail.com>
2022-02-07 19:12:57 +0600

    Modules

---
+++ modules/functions.py
@@ -0,0 +1,6 @@
+def print_name(first_name, last_name):
+    return f"{first_name} {last_name}"
+
+def add(num_one, num_two):
+    sum = num_one + num_two
+    return sum
---
+++ modules/main.py
@@ -0,0 +1,8 @@
+# import functions
+from functions import print_name, add
+
+# print(functions.print_name("Nafio", "Nabi"))
+# print(functions.add(25, 25))
+
+print(print_name("Nafio", "Nabi"))
+print(add(25, 25))
